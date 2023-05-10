078797f249cd01070f4974f4aa3a14c7c4084da1
eallenOP <elise.allen@op.ac.nz>
2023-05-10 13:47:59 +1200

    Hide the auth token from the client

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,11 +1,14 @@
 /* This file demonstrates one official option for fetching in SvelteKit, including hiding a token from the front end. Quick reference followed from https://svelte.deepwebworks.com/blog/kit2 */
 
+import { API_KEY } from '$env/static/private';
+console.log(API_KEY);
+
 // Put these paramaters here in constants to keep things tidy
 const url = 'https://api.spacetraders.io/v2/my/agent';
 const options = {
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiTEFKWEVMIiwiaWF0IjoxNjgzNjc5NTM5LCJzdWIiOiJhZ2VudC10b2tlbiJ9.PMtclg0P-o4amSUk--8yE5U0rO_KWaDlGthHlD-ziEHOgHCv8DKp6yT46V3FznLHzp1GSDYdnuPKcyUrUDgSmY23MFqjaKmt3Xw_BEuz_Lz2BAeiYrAb19dqRUAdzV5zxExbi5A07xVmsL37bMLyxFpFHntbecIhRaJb6pVts8Q1x-gRZIJzsd73OaFHSr-_d8ESM4ZGxts9T7XtfyQPn5ZsclfEAEQoWnlN6wgiUdvfv4Fz3cdL5sEJ4Vlzw5WH1D-5euPiZCCvuIGsHI7aZJHOSrMWTFBH8C4lldh67rNFnGTBExtGgC6USxZ6vGG7ijO2lE_v0J5fiQvVmJyqU8B1clEa-KQ20_iZGeGpuW6QlFUwECi24W5NJVjrik2hXiRYCpgePE1v_pHsFtm0Hk4OShCrhkcCU32BTayugMb0CYjUbzZwwpKb94mkdybO8adsR03AOwo9v3fL9qy7lgnyyQmncHUyie_wcmyIyDjkrzmV7MqC4LKMpx-Nu9CG'
+    'Authorization': API_KEY
   },
 };
 
@@ -16,6 +19,7 @@ export async function load() {
 
   // Check that it worked and return it so the page can use it, or give us a page not found status
   if (agent_details) {
+    console.log(agent_details);
     return {
       agent_details,
     };
