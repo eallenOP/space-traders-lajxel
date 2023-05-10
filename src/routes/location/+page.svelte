b0d08a1eda658a28391c49ecaaf1de5e094ea59a
eallenOP <elise.allen@op.ac.nz>
2023-05-10 14:35:58 +1200

    Display location features

--- src/routes/location/+page.svelte
+++ src/routes/location/+page.svelte
@@ -1,9 +1,24 @@
 <script>
-export let data;
-const location = data.location_details.data;
+  // Get the data fetched on the server and store it somewhere tidy
+  export let data;
+  const location = data.location_details.data;
 </script>
 
 <a href="/">back</a>
 
 <h1>Location: {location.symbol}</h1>
-<h2>Type: {location.type}</h2>
+<h2>Type: {location.type}; Faction: {location.faction.symbol}</h2>
+
+<h3>Orbitals:</h3>
+<ul>
+  {#each location.orbitals as orbital}
+    <li>{orbital.symbol}</li>
+  {/each}
+</ul>
+
+<h3>Location traits:</h3>
+<ul>
+  {#each location.traits as trait}
+    <li>{trait.name}</li>
+  {/each}
+</ul>
