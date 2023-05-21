2f05136707c73951075f8ffb09c24a90ac9751fb
eallenop <eallen@op.ac.nz>
2023-05-22 11:50:34 +1200

    Display agent's contracts

---
+++ src/routes/contracts/+page.server.js
@@ -0,0 +1,23 @@
+import { API_KEY } from '$env/static/private';
+
+// Put these paramaters here in constants to keep things tidy
+const url = 'https://api.spacetraders.io/v2/my/contracts';
+const options = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': API_KEY
+  },
+};
+
+// Async load function is needed so that this runs when the corresponding page loads
+export async function load({ fetch }) {
+  const response = await fetch(url, options); // Run the fetch and bung the result in a variable
+  const contracts = await response.json(); // Turn the response (once we have one) into usable json format
+
+  // Check that it worked and return it so the page can use it, or give us a page not found status
+  if (!contracts) return {    status: 404,  };
+    console.log(contracts);
+    return {
+      contracts,
+    };
+  }
---
+++ src/routes/contracts/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let data;
+	const contracts = data.contracts.data;
+	const meta = data.contracts.meta;
+</script>
+
+<h1>Contracts</h1>
+
+<!-- Loop over the contracts -->
+{#each contracts as contract}
+	<div class="contract-box">
+		<h2>ID: {contract.id}</h2>
+		<p>{contract.type}</p>
+		<p>Deadline: {contract.deadlineToAccept}</p>
+		<h3>Terms:</h3>
+		<p>Deliver by: {contract.terms.deadline}</p>
+		<!-- Loop over delivery terms -->
+		<ul>
+			{#each contract.terms.deliver as delivery}
+				<li>{delivery.tradeSymbol}: {delivery.unitsRequired}</li>
+			{/each}
+		</ul>
+		<h3>
+            Status:  
+        </h3>
+            {#if contract.accepted}
+				<p>Accepted </p>
+			{:else if contract.fulfilled}
+				<p>and fulfilled</p>
+			{:else}
+				<p>Not Accepted</p>
+			{/if}
+	</div>
+{/each}
+
+<style>
+	.contract-box {
+		border: 1px solid black;
+		padding: 10px;
+		margin: 10px;
+	}
+	.contract-box h2 {
+		font-weight: bold;
+	}
+	.contract-box h3 {
+		font-size: 1em;
+	}
+</style>
