681cfef1c1b9b81b213c18a5c07799702cffa5d1
eallen <elise.allen@op.ac.nz>
2024-05-21 13:28:42 +1200

    Create +page.svelte for location route

---
+++ src/routes/location/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+/* The url of this page is /location
+   Sub-pages are generated from the location data and are at /location/sub-page
+   where "sub-page" is replaced with the name of each location
+*/
+</script>
+
+<h1>"Location" route main page</h1>
+
